d6098f8a939eb08f1b14adbcc608ad56ae5c2a9c
kev7862 <kevmatite7862@gmail.com>
2019-03-09 19:16:39 +0300

    Embeded instagram feed and and about us card

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '81e978b3204453adbefa04b2657a0f9d',
+        accessToken: '2019-03-09 15:33:12 UTC'
+    });
+    userFeed.run();
+</script>
