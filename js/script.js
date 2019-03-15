8e6f948c4f841bf93b6dd1a7640d6e4489944760
kev7862 <kevmatite7862@gmail.com>
2019-03-16 00:07:24 +0300

    Finished the Mission and Vision Section. everything looks good!

--- js/script.js
+++ js/script.js
@@ -1,24 +1,52 @@
 $(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
 
-        $('#back-to-top').tooltip('show');
 
-        swal("Here's the title!", "...and here's the text!");
 
-        
+        // Tween-Max Section
+        var navbarResponsive = document.getElementById('navbarResponsive');
+        TweenMax.from(navbarResponsive, 1, {x:200, autoAlpha:1});
+
+        // ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+var ourScene = new ScrollMagic.Scene({
+  triggerElement: '.pro'
+})
+.setClassToggle('.pro', 'fade-in');
+
+//teleprompter Section
+var tele = document.getElementById('tele');
+$("#tele").teleprompter();
+
+
+
+
+
+
+
+
+
+
+
+//Back - to - Top Section
+        $(window).scroll(function () {
+               if ($(this).scrollTop() > 50) {
+                   $('#back-to-top').fadeIn();
+               } else {
+                   $('#back-to-top').fadeOut();
+               }
+           });
+           // scroll body to 0px on click
+           $('#back-to-top').click(function () {
+               $('#back-to-top').tooltip('hide');
+               $('body,html').animate({
+                   scrollTop: 0
+               }, 800);
+               return false;
+           });
+
+           $('#back-to-top').tooltip('show');
+
+
 
 });
